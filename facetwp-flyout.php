3e8749d2811a1d082e9d333247858da4eecf8d7d
mgibbs189 <mgibbs189@gmail.com>
2019-02-15 20:54:23 -0500

    Tagged 0.2.1

--- facetwp-flyout.php
+++ facetwp-flyout.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: FacetWP - Flyout menu
 Description: Displays a flyout facet menu
-Version: 0.2
+Version: 0.2.1
 Author: FacetWP, LLC
 Author URI: https://facetwp.com/
 GitHub URI: facetwp/facetwp-flyout
